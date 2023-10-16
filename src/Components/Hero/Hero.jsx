60d5f82cc70fecfdae235fdd765cbbd884aa6699
KartikLakra1 <kitrakarkal@gmail.com>
2023-10-16 22:58:49 +0530

    nav bar and footer added in the shop section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./hero.css";
+import './hero.css';
 import hand_icon from "../Assets/hand_icon.png";
 import arrow_icon from '../Assets/arrow.png';
 import hero_image from '../Assets/hero_image.png'
@@ -10,18 +10,19 @@ const Hero = () => {
       <div className="hero-left">
         <h2>NEW ARRIVALS ONLY</h2>
         <div>
-          <div className="hand-hand-icon">
+          <div className="hero-hand-icon">
             <p>new</p>
             <img src={hand_icon} alt="hand-icon" />
           </div>
           <p>Collections</p>
           <p>For everyone</p>
         </div>
-        <div className="hero-latest-button">
+        <div className="hero-latest-btn">
         <div>Latest Collection</div>
         <img src={arrow_icon} alt="arrow-icon" />
         </div>
       </div>
+
       <div className="hero-right">
         <img src={hero_image} alt="hero-image"/>
       </div>
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
+import NewCollection from "../Components/NewCollections/NewCollection";
+import Newletter from "../Components/Newsletter/Newletter";
+import Offers from "../Components/Offers/Offers";
+import Popular from "../Components/Popular/Popular";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Popular />
+      <Offers />
+      <NewCollection />
+      <Newletter />
     </div>
   )
 };
