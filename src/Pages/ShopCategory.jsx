7878f99a6db6563bca3366183a55d8fc9f6c4b92
KartikLakra1 <kitrakarkal@gmail.com>
2023-10-17 09:37:49 +0530

    different pages added using react-router-dom

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
+import "./CSS/Shopcategory.css";
+import all_product from "./../Components/Assets/all_product";
+import { ShopContext } from "./../Context/ShopContext";
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import Item from "../Components/Items/Item";
 
-const ShopCategory = () => {
-  return <div>ShopCategory</div>;
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+  return (
+    <div className="shop-category">
+      <img className="shopcategory-banner" src={props.banner} alt="banner" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1 - 12 </span> out of 36 Products.
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="drop-down-icon" />
+        </div>
+      </div>
+
+      <div className="shopcategory-products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+      Explore More
+      </div>
+    </div>
+  );
 };
 
 export default ShopCategory;
